f6d5f5804c3aba7d9145f67b69a25f28ea807633
PraneshS1009 <97275546+PraneshS1009@users.noreply.github.com>
2022-03-02 15:51:53 +0530

    added3

--- exp1/exp1.js
+++ exp1/exp1.js
@@ -1,1 +1,23 @@
 console.log("Fine");
+
+var toppings = ["mushroom","tomatoes",,"eggs","chili","lettuce","avacado","chiles","bacon","pickles","onions"];
+console.log(toppings);
+
+var feedback = [
+    { comment : 'Love hte burger', rating: 4},
+    { comment : 'Horrible service', rating: 2},
+    { comment : 'Smoothies are great ,Liked the burger too', rating: 5},
+    { comment : 'Ambiance needs work', rating: 3},
+    { comment : 'I dont like burgers', rating: 1},
+]
+console.log(feedback);
+
+function findBurgRating(singleFeedback){
+    if(singleFeedback.comment.includes('burg')){
+        return true;
+    }
+}
+var burgrating = feedback.find(findBurgRating);
+console.log(burgrating);
+
+
