491d618ee32f28f8ce6973a39a7e667b572993b6
PraneshS1009 <97275546+PraneshS1009@users.noreply.github.com>
2022-03-02 13:15:19 +0530

    added

--- Objects/exp1.js
+++ Objects/exp1.js
@@ -1,1 +1,21 @@
 console.log("Working");
+
+const nothing = {
+    age :100,
+    name:'Mr',
+    work:'Developer',
+    place:'Erode',
+    deg:{
+        diploma:'75%',
+        engg:'7.65',
+    }
+};
+console.log(nothing);
+
+nothing.age = 22;
+console.log(nothing);
+
+let nothing1 = nothing;
+
+nothing1.name = 'Prabhu';
+console.log(nothing1, nothing);
